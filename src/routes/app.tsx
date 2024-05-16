180a259a0212efd558678dab21def276b87d49da
Meacronacer <sinnerscrown09@gmail.com>
2024-05-16 14:13:23 +0300

    fixed 404 problem

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -7,10 +7,11 @@ import HomePage from "../pages/homePage";
 const App = () => {
   return (
     <Routes>
-      <Route path="/shop-co/" element={<HomePage />} />
-      <Route path="/shop-co/:productTitle" element={<ProductDetailPage />} />
-      <Route path="/shop-co/shop" element={<CategoryPage />} />
-      <Route path="/shop-co/cart" element={<CartPage />} />
+      <Route path="shop-co/" element={<HomePage />} />
+      <Route path="shop-co/:productTitle" element={<ProductDetailPage />} />
+      <Route path="shop-co/shop" element={<CategoryPage />} />
+      <Route path="shop-co/cart" element={<CartPage />} />
+      <Route path='*' element={<div><h1>PAGE NOT FOUND 404</h1></div>} />
     </Routes>
   );
 };
